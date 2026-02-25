a53b28916fc879e6e7e0422e5b0eba3e80562d54
Tyler Stuyfzand <tyler@auroradev.org>
2026-02-25 18:09:44 -0500

    [feature] Add bulk message retrieval, start of embed (#3)

--- embed.go
+++ embed.go
@@ -1,5 +1,25 @@
 package valourgo
 
+import "encoding/json"
+
+type EmbedItemType int
+
+const (
+	EmbedTypeText EmbedItemType = 1 + iota
+	EmbedTypeButton
+	EmbedTypeInputBox
+	EmbedTypeTextArea
+	EmbedTypeProgressBar
+	EmbedTypeForm
+	EmbedTypeGoTo
+	EmbedTypeDropDownItem
+	EmbedTypeDropDownMenu
+	EmbedTypeEmbedRow
+	EmbedTypeEmbedPage
+	EmbedTypeProgress
+	EmbedTypeMedia
+)
+
 type Embed struct {
 	Id                   string `json:"id"`
 	Name                 string `json:"name"`
@@ -9,12 +29,55 @@ type Embed struct {
 	Pages []EmbedPage `json:"pages"`
 }
 
-type EmbedItem struct {
-	Children []EmbedItem `json:"children"`
+type EmbedItem interface {
+	Type() EmbedItemType
 }
 
 type EmbedPage struct {
-	EmbedItem
-	Title  string `json:"title"`
-	Footer string `json:"footer"`
+	Children []EmbedItem `json:"children"`
+	Title    string      `json:"title"`
+	Footer   string      `json:"footer"`
+}
+
+func (e EmbedPage) Type() EmbedItemType {
+	return EmbedTypeEmbedPage
+}
+
+func (e EmbedPage) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]any{
+		"type":     EmbedTypeEmbedPage,
+		"children": e.Children,
+		"title":    e.Title,
+		"footer":   e.Footer,
+	})
+}
+
+type EmbedRow struct {
+	Children []EmbedItem
+}
+
+func (e EmbedRow) Type() EmbedItemType {
+	return EmbedTypeEmbedRow
+}
+
+func (e EmbedRow) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]any{
+		"type":     EmbedTypeEmbedRow,
+		"children": e.Children,
+	})
+}
+
+type EmbedText struct {
+	Text string `json:"text"`
+}
+
+func (e EmbedText) Type() EmbedItemType {
+	return EmbedTypeText
+}
+
+func (e EmbedText) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]any{
+		"type": EmbedTypeText,
+		"text": e.Text,
+	})
 }
--- message.go
+++ message.go
@@ -2,13 +2,24 @@ package valourgo
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
 )
+
+// LatestMessageIndex is long.MaxValue in C#, this will return our latest messages only
+const (
+	LatestMessageIndex = MessageID(9223372036854775807)
+	maxMessageLimit    = 64
+)
+
+var ErrInvalidCount = errors.New("invalid message count")
 
 type Message struct {
 	ID              MessageID           `json:"id"`
@@ -41,8 +52,9 @@ type SendMessageData struct {
 	ChannelID       ChannelID            `json:"channelId"`
 	ReplyToID       *MessageID           `json:"replyToId"`
 	Content         string               `json:"content"`
-	Attachments     []*MessageAttachment `json:"attachments"`
-	AttachmentsData string               `json:"attachmentsData"`
+	Attachments     []*MessageAttachment `json:"attachments,omitempty"`
+	AttachmentsData string               `json:"attachmentsData,omitempty"`
+	EmbedData       string               `json:"embedData,omitempty"`
 	Fingerprint     string               `json:"fingerprint"`
 }
 
@@ -52,6 +64,70 @@ type EditMessageData struct {
 	Content  *string   `json:"content"`
 }
 
+// Messages retrieves the latest x messages
+func (n *Node) Messages(planetID PlanetID, channelID ChannelID, limit uint) ([]Message, error) {
+	return n.MessagesBefore(planetID, channelID, LatestMessageIndex, limit)
+}
+
+// MessagesBefore retrieves messages before a specific message
+func (n *Node) MessagesBefore(planetID PlanetID, channelID ChannelID, index MessageID, limit uint) ([]Message, error) {
+	msgs := make([]Message, 0, limit)
+
+	fetch := uint(maxMessageLimit)
+
+	unlimited := limit == 0
+
+	for limit > 0 || unlimited {
+		if limit > 0 {
+			fetch = uint(intMin(maxMessageLimit, int(limit)))
+			limit -= maxMessageLimit
+		}
+
+		m, err := n.messagesBefore(planetID, channelID, index, fetch)
+
+		if err != nil {
+			return msgs, err
+		}
+
+		msgs = append(msgs, m...)
+
+		if len(m) < maxMessageLimit {
+			break
+		}
+
+		index = m[len(m)-1].ID
+	}
+
+	if len(msgs) == 0 {
+		return nil, nil
+	}
+
+	return msgs, nil
+}
+
+// messagesBefore is called to retrieve messages, used with MessagesBefore to append to a slice
+func (n *Node) messagesBefore(planetID PlanetID, channelID ChannelID, index MessageID, limit uint) ([]Message, error) {
+	switch {
+	case limit == 0:
+		limit = 50
+	case limit > 64:
+		limit = 64
+	}
+
+	var messages []Message
+
+	v := make(url.Values)
+	v.Set("index", index.String())
+	v.Set("count", strconv.FormatUint(uint64(limit), 10))
+
+	if err := n.requestJSON(http.MethodGet, planetID.Route("channels", channelID.String(), "messages"), nil, &messages); err != nil {
+		return nil, err
+	}
+
+	return messages, nil
+}
+
+// Message retrieves a single message
 func (n *Node) Message(id MessageID) (*Message, error) {
 	var message Message
 
@@ -66,6 +142,7 @@ func (n *Node) Message(id MessageID) (*Message, error) {
 	return &message, nil
 }
 
+// EditMessage updates a message
 func (n *Node) EditMessage(id MessageID, m EditMessageData) (*Message, error) {
 	m.ID = id
 
@@ -82,6 +159,7 @@ func (n *Node) EditMessage(id MessageID, m EditMessageData) (*Message, error) {
 	return &updatedMessage, nil
 }
 
+// DeleteMessage deletes a message
 func (n *Node) DeleteMessage(id MessageID) error {
 	res, err := n.request(http.MethodDelete, id.Route(), nil)
 
@@ -98,12 +176,14 @@ func (n *Node) DeleteMessage(id MessageID) error {
 	return fmt.Errorf("unknown status %d", res.StatusCode)
 }
 
+// SendMessage sends a simple text message
 func (n *Node) SendMessage(planetID PlanetID, channelID ChannelID, content string) (*Message, error) {
 	return n.SendMessageComplex(planetID, channelID, SendMessageData{
 		Content: content,
 	})
 }
 
+// SendMessageComplex sends a message with optional text, attachments, and embeds
 func (n *Node) SendMessageComplex(planetID PlanetID, channelID ChannelID, send SendMessageData) (*Message, error) {
 	send.PlanetID = planetID
 	send.ChannelID = channelID
@@ -162,4 +242,13 @@ func (n *Node) SendMessageComplex(planetID PlanetID, channelID ChannelID, send S
 	}
 
 	return &m, nil
+}
+
+// intMin is a simple math.Min for integers
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
 }
